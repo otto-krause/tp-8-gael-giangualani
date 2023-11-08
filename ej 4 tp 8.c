5c66aab5e0912c37c8a146c75c04586ca9d8e59d
Giangualani12 <141842179+Giangualani12@users.noreply.github.com>
2023-11-07 23:09:46 -0300

    Add files via upload

---
+++ ej 4 tp 8.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int ma[3][3],i,j,kit,kano,orden ;
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			
+			printf("Ingrese valores enteros para  : fila %d, columna %d: ", i+1,j+1);
+				scanf("%d", &ma[i][j]);
+		}
+	}
+
+	
+	for(i=0;i<3;i++) {
+		
+		for(j=0;j<3;j++) {
+			
+			for(kit=0;kit<3;kit++) {
+				
+				for(kano=0;kano<3;kano++) {
+					
+					if (ma[i][j]<ma[kit][kano])
+					{
+						orden =ma[i][j];
+						ma[i][j]=ma[kit][kano];
+						ma[kit][kano]=orden ;
+					}
+				}
+			}
+		}
+	}
+	
+	
+	printf("Matriz ordenada:\n");
+	
+	for (i=0;i<3;i++) {
+		for (j=0;j<3;j++) {
+			
+			printf("%d", ma[i][j]);
+		}
+		printf("\n");
+	}
+	system("pause");
+	return 0;
+}
